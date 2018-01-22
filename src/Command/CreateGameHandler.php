71173c89c325bad6089699ce479ae959e2792da6
Bjorn <bjorn@sumocoders.be>
2018-01-22 17:34:40 +0100

    Download images to assets directory

--- src/Command/CreateGameHandler.php
+++ src/Command/CreateGameHandler.php
@@ -4,18 +4,24 @@ namespace App\Command;
 
 use App\Entity\Game;
 use App\Repository\GameRepository;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class CreateGameHandler
 {
     /** @var GameRepository */
     private $gameRepository;
 
+    /** @var KernelInterface */
+    private $kernel;
+
     /**
-     * @param GameRepository $gameRepository
+     * @param GameRepository  $gameRepository
+     * @param KernelInterface $kernel
      */
-    public function __construct(GameRepository $gameRepository)
+    public function __construct(GameRepository $gameRepository, KernelInterface $kernel)
     {
         $this->gameRepository = $gameRepository;
+        $this->kernel = $kernel;
     }
 
     /**
@@ -23,6 +29,14 @@ class CreateGameHandler
      */
     public function handle(CreateGame $createGame): void
     {
+        $image = $createGame->getImage();
+        if (!file_exists($createGame->getImage())) {
+            $image = $this->downloadImage(
+                $createGame->getImage(),
+                (string) $createGame->getUuid()
+            );
+        }
+
         $this->gameRepository->save(
             new Game(
                 $createGame->getUuid(),
@@ -31,12 +45,36 @@ class CreateGameHandler
                 $createGame->getMinPlayers(),
                 $createGame->getMaxPlayers(),
                 $createGame->getPublishedOn(),
-                $createGame->getImage(),
+                $image,
                 $createGame->getArtists(),
                 $createGame->getDesigners(),
                 $createGame->getPublishers(),
                 $createGame->getBoardGameGeekId()
             )
         );
+    }
+
+    /**
+     * @param string $image
+     * @param string $destination
+     *
+     * @return string
+     */
+    private function downloadImage(string $image, string $destination): string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_URL, $image);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $imageData = curl_exec($ch);
+        curl_close($ch);
+
+        $path = '/assets/images/games/'.$destination;
+
+        $saveFile = fopen($this->kernel->getRootDir().'/../public'.$path, 'w');
+        fwrite($saveFile, $imageData);
+        fclose($saveFile);
+
+        return $path;
     }
 }
